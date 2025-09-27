7281d34fd9c2aebe38a999a61e0fedcafb8c4256
Rudrashukla2005 <shuklarudra2005@gmail.com>
2025-09-27 12:38:55 +0530

    Update app5.js file

--- app5.js
+++ app5.js
@@ -1,1 +1,19 @@
+const express = require('express')
 
+const app = express()
+const port = 4000
+
+app.set('views',__dirname+'/views')
+app.set('view engine','ejs')
+
+app.get('/', (req,res)=> {
+    res.send("Hii Rudra Shukla!!!")
+})
+
+app.get('/homedashboard', (req,res)=> {
+    res.render('homedashboard', {name: "Rudra"})
+})
+
+app.listen(port, ()=> {
+    console.log(`App5 Running on Port ${port}`)
+})
